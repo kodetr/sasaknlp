9b0c01f7a1918d03c4b73fcac5ad7458732660e8
kodetr <t4nwi12@gmail.com>
2026-09-11 14:32:00 +0800

    chore: bersihkan build cache dan terapkan gitignore standar

--- build/lib/sasaknlp/dialect/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Dialect module for SasakNLP."""
-
-from sasaknlp.dialect.detector import DialectDetector
-from sasaknlp.dialect.manager import DialectManager
-
-__all__ = [
-    "DialectManager",
-    "DialectDetector",
-]
--- build/lib/sasaknlp/tokenizer/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Tokenizer module for SasakNLP."""
-
-from sasaknlp.tokenizer.sentence_tokenizer import SasakSentenceTokenizer
-from sasaknlp.tokenizer.tokenizer import SasakTokenizer
-
-__all__ = [
-    "SasakTokenizer",
-    "SasakSentenceTokenizer",
-]
